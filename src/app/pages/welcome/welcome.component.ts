aab074e878eeed6561bff6e1a25a3164e56b310b
Tércio Bianchi <terciobianchi@gmail.com>
2021-02-20 09:14:31 -0300

    Ajust layout

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
+import { filter, distinctUntilChanged } from 'rxjs/operators';
 
 import { NzTableFilterFn, NzTableFilterList, NzTableSortFn, NzTableSortOrder } from 'ng-zorro-antd/table';
 import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 
-
+interface IBreadCrumb {
+  label: string;
+  url: string;
+}
 interface DataItem {
   name: string;
   age: number;
@@ -27,6 +32,8 @@ interface ColumnItem {
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+
+  breadcrumbs: IBreadCrumb[] = [];
 
   validateForm!: FormGroup;
 
@@ -115,9 +122,24 @@ export class WelcomeComponent implements OnInit {
     this.validateForm.controls.captcha.setValue('123133')
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder) {
+      this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
+      console.log(this.breadcrumbs);
+  }
+
 
   ngOnInit(): void {
+    this.router.events.pipe(
+      filter(ev => ev instanceof NavigationEnd),
+      distinctUntilChanged(),
+    ).subscribe(() => {
+      this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
+    })
+
+
     this.validateForm = this.fb.group({
       email: [null, [Validators.email, Validators.required]],
       password: [null, [Validators.required]],
@@ -130,6 +152,45 @@ export class WelcomeComponent implements OnInit {
       captcha: [null, [Validators.required]],
       agree: [false]
     });
+  }
+
+  /**
+   * Recursively build breadcrumb according to activated route.
+   * @param route
+   * @param url
+   * @param breadcrumbs
+   */
+  buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: IBreadCrumb[] = []): IBreadCrumb[] {
+    //If no routeConfig is avalailable we are on the root path
+    let label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data.breadcrumb : '';
+    let isClickable = route.routeConfig && route.routeConfig.data && route.routeConfig.data.isClickable;
+    let path = route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
+
+    // If the route is dynamic route such as ':id', remove it
+    const lastRoutePart = path && path.split('/').pop();
+    const isDynamicRoute = lastRoutePart && lastRoutePart.startsWith(':');
+    if(path && lastRoutePart && isDynamicRoute && !!route.snapshot) {
+      const paramName = lastRoutePart.split(':')[1];
+      path = path.replace(lastRoutePart, route.snapshot.params[paramName]);
+      label = route.snapshot.params[paramName];
+    }
+
+    //In the routeConfig the complete path is not available,
+    //so we rebuild it each time
+    const nextUrl = path ? `${url}/${path}` : url;
+
+    const breadcrumb: IBreadCrumb = {
+        label: label,
+        url: nextUrl,
+    };
+    // Only adding route with non-empty label
+    const newBreadcrumbs = breadcrumb.label ? [ ...breadcrumbs, breadcrumb ] : [ ...breadcrumbs];
+    if (route.firstChild) {
+        //If we are not on our current path yet,
+        //there will be more children to look after, to build our breadcumb
+        return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+    }
+    return newBreadcrumbs;
   }
 
 }
